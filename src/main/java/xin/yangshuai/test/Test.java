296da02856da1737ae33b8bfa49ba77a9dee3af7
shuai <1627392348@qq.com>
2019-03-05 18:10:01 +0800

    参考

--- src/main/java/xin/yangshuai/test/Test.java
+++ src/main/java/xin/yangshuai/test/Test.java
@@ -8,7 +8,15 @@ package xin.yangshuai.test;
  */
 public class Test {
 	public static void main(String[] args) {
-		int a = -10/3;
+		int a = -10 / 3;
 		System.out.println(a);
+
+		String str1 = "aaa";
+		String str2 = "bbb";
+		String str3 = "aaabbb";
+		String str5 = "aaa" + "bbb";
+		String s = "123";
+		String s1 = "123";
+		System.out.println("aaa" + "bbb" == str3);
 	}
 }
